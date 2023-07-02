d5e456dd0a249abf3b1a710a518a329b9f960fe3
Rishi9425 <116002842+Rishi9425@users.noreply.github.com>
2023-07-03 00:16:49 +0530

    updated

--- server.js
+++ server.js
@@ -16,8 +16,10 @@ dotenv.config();
 connectDB();
 
 //esmodule fix
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 //rest object
 const app = express();
 
